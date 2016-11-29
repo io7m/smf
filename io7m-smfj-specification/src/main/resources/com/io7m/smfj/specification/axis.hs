73d88cd17c512a38328bf7816cd2729a636d7cfd
Mark Raynsford <code@io7m.com>
2016-11-29 12:20:05 +0000

    Improve coordinate system specification

--- io7m-smfj-specification/src/main/resources/com/io7m/smfj/specification/axis.hs
+++ io7m-smfj-specification/src/main/resources/com/io7m/smfj/specification/axis.hs
@@ -6,11 +6,32 @@ data Axis
   | NegativeY
   | NegativeZ
 
+data AxisName
+  = AxisX
+  | AxisY
+  | AxisZ
+
+axisName :: Axis -> AxisName
+axisName PositiveX = AxisX
+axisName NegativeX = AxisX
+axisName PositiveY = AxisY
+axisName NegativeY = AxisY
+axisName PositiveZ = AxisZ
+axisName NegativeZ = AxisZ
+
 data AxisSystem = AxisSystem {
   axis_right   :: Axis,
   axis_up      :: Axis,
   axis_forward :: Axis
 }
+
+axisValid :: (Axis, Axis, Axis) -> Bool
+axisValid (right, up, forward) =
+  case (axisName right, axisName up, axisName forward) of
+    (AxisX, AxisY, AxisZ) -> True
+    (AxisZ, AxisX, AxisY) -> True
+    (AxisY, AxisZ, AxisX) -> True
+    _                     -> False
 
 data WindingOrder
   = WindingOrderClockwise
