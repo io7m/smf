0e0b690aa8aefd630c1b7b67ba9cdf53202ba7b3
Mark Raynsford <code@io7m.com>
2017-01-10 11:33:12 +0000

    Zero pad schema ID values
    The intention was to zero pad originally, but the wrong format
    specifier was used in the serializer.
    
    Fix #30

--- io7m-smfj-format-text/src/main/java/com/io7m/smfj/format/text/SMFTV1Serializer.java
+++ io7m-smfj-format-text/src/main/java/com/io7m/smfj/format/text/SMFTV1Serializer.java
@@ -206,7 +206,7 @@ final class SMFTV1Serializer implements SMFSerializerType
       if (schema_id.vendorID() != 0) {
         this.writer.append(
           String.format(
-            "schema %8x %8x %d %d",
+            "schema %08x %08x %d %d",
             Integer.valueOf(schema_id.vendorID()),
             Integer.valueOf(schema_id.schemaID()),
             Integer.valueOf(schema_id.schemaMajorVersion()),
